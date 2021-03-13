fbbe21a7322436c9671537a9340f401507f39cf6
Andrew <andrewscaldwell@gmail.com>
2021-03-12 21:09:54 -0500

    a bunch of dumb random methods

--- myfile.py
+++ myfile.py
@@ -1,8 +1,17 @@
 def runMeNOW():
     print('hello world')
-    x = 1-0
+    x = 33-0
     print(x)
     
 
 def newMethod():
     print('running new method')
+
+
+def myMethodNew():
+    x =10
+    x = x - 1
+    print(x)
+
+def newCoolerMethod():
+    print('cool epic new cooler')
