ce1b4e4d3e0d0772f2c84f554825369a795f02e7
arifire21 <ariwolfpup@icloud.com>
2021-03-12 21:12:05 -0500

    commit

--- myfile.py
+++ myfile.py
@@ -1,6 +1,11 @@
 def runMe():
     print('hello world')
 
-def oldMethod():
-    print('this is an old method')
+# def oldMethod():
+#     print('this is an old method')
 
+def test():
+    print('dog')
+
+def test2()
+    print('dog2')
